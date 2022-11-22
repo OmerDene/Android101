cffe7989666dd266a1f25478ae77dab6e0b07024
OmerDene <omerdene@gmail.com>
2022-11-22 22:57:12 +0300

    basit kenny' yakalama yapildi

---
+++ app/src/main/java/com/example/android101/Kennyyy.java
@@ -0,0 +1,14 @@
+package com.example.android101;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Kennyyy extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kennyyy);
+    }
+}
