72eb265a0a94c917d531b2ede76d5b2fa16a8fb0
OmerDene <omerdene@gmail.com>
2022-11-23 23:34:17 +0300

    Oyunu tamamladık

--- app/src/main/java/com/example/android101/OOPProject.java
+++ app/src/main/java/com/example/android101/OOPProject.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class CatchTheKenny extends AppCompatActivity {
+public class OOPProject extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_catch_the_kenny);
+        setContentView(R.layout.activity_oopproject);
     }
 }
