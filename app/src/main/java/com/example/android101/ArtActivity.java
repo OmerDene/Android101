248294b28296902bc633b3cdb8a76f7f27c056a3
OmerDene <omerdene@gmail.com>
2022-11-29 21:45:30 +0300

    ArtBook appi menu olusturuldu layout ile main activity kodları birlestirildi

---
+++ app/src/main/java/com/example/android101/ArtActivity.java
@@ -0,0 +1,14 @@
+package com.example.android101;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ArtActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_art);
+    }
+}
