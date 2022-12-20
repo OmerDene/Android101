63c923bcadd3882cfeeb9b8120a648407f53a3ca
OmerDene <omerdene@gmail.com>
2022-12-20 21:32:52 +0300

    update

--- app/src/main/java/com/example/android101/view/JavaMapsMain.java
+++ app/src/main/java/com/example/android101/view/JavaMapsMain.java
@@ -2,21 +2,57 @@ package com.example.android101.view;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.room.Room;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 
+import com.example.android101.MainActivity;
 import com.example.android101.R;
+import com.example.android101.databinding.ActivityJavaMapsBinding;
+import com.example.android101.model.Place;
+import com.example.android101.roomdb.PlaceDao;
+import com.example.android101.roomdb.PlaceDatabase;
+
+import java.util.List;
+
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
+import io.reactivex.rxjava3.core.Scheduler;
+import io.reactivex.rxjava3.disposables.CompositeDisposable;
+import io.reactivex.rxjava3.schedulers.Schedulers;
 
 public class JavaMapsMain extends AppCompatActivity {
+    private ActivityJavaMapsBinding binding;
+    private CompositeDisposable compositeDisposable = new CompositeDisposable();
+    PlaceDatabase db;
+    PlaceDao placeDao;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_java_map_mainactivity);
+        binding = ActivityJavaMapsBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+        db = Room.databaseBuilder(getApplicationContext(),PlaceDatabase.class,"Places").build();
+        placeDao = db.placeDao();
+        compositeDisposable.add(placeDao.getAll()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(JavaMapsMain.this::handleResponse)
+
+
+        );
+
+
+    }
+    private  void handleResponse(List<Place> placeList) {
+
+
 
     }
 
@@ -34,5 +70,10 @@ public class JavaMapsMain extends AppCompatActivity {
             startActivity(intent);
         }
         return super.onOptionsItemSelected(item);
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        compositeDisposable.clear();
     }
 }
