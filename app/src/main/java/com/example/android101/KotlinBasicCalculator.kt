6d9716e42315e157faa8d2cc5103c7b81f8daec4
OmerDene <omerdene@gmail.com>
2023-03-28 23:56:07 +0300

    kotlinCalculator

---
+++ app/src/main/java/com/example/android101/KotlinBasicCalculator.kt
@@ -0,0 +1,13 @@
+package com.example.android101
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class KotlinLearning : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_kotlin_learning)
+
+    }
+}
