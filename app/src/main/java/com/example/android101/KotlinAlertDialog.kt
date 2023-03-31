6c96b0dee3e68331d4f4df0c08f880d1b45244b7
OmerDene <omerdene@gmail.com>
2023-03-31 14:51:02 +0300

    KotlinAlertDialog

---
+++ app/src/main/java/com/example/android101/KotlinAlertDialog.kt
@@ -0,0 +1,35 @@
+package com.example.android101
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+
+class KotlinAlertDialog : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_kotlin_alert_dialog)
+    }
+    fun save(view : View) {
+
+        val alert = AlertDialog.Builder(this)
+        alert.setTitle("Save")
+        alert.setMessage("Are You Sure?")
+
+        alert.setPositiveButton("Yes") {dialog, which ->
+
+            //OnClick Listener
+            Toast.makeText(this,"Saved",Toast.LENGTH_LONG).show()
+        }
+        alert.setNegativeButton("No") {dialog, which ->
+
+            //OnClick Listener
+            Toast.makeText(applicationContext,"Not Saved",Toast.LENGTH_LONG).show()
+
+        }
+
+        alert.show()
+
+    }
+}
--- app/src/main/java/com/example/android101/KotlinIntentContext.kt
+++ app/src/main/java/com/example/android101/KotlinIntentContext.kt
@@ -10,11 +10,14 @@ class KotlinIntentContext : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_kotlin_intent_context)
+
+
         val getIntent = intent
         val name = getIntent.getStringExtra("name")
         val surname = getIntent.getStringExtra("surname")
         nameShowText.text="İsminiz :" + name
         surnameShowText.text="soyisminiz : ${surname}"
+
 
 
     }
