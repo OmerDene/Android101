3e3b005e3641d4d0af2c01130aa897c299c7f216
OmerDene <omerdene@gmail.com>
2023-04-03 21:06:01 +0300

    countDowntimer and alertDialog defined

--- app/src/main/java/com/example/android101/KotlinCatchGame.kt
+++ app/src/main/java/com/example/android101/KotlinCatchGame.kt
@@ -1,11 +1,56 @@
 package com.example.android101
 
+import android.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_kotlin_catch_game.*
 
 class KotlinCatchGame : AppCompatActivity() {
+    private var score = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_kotlin_catch_game)
+        object :CountDownTimer(15000,1000){
+            override fun onTick(p0: Long) {
+
+                timeText.text ="Sure : ${p0/1000}"
+
+            }
+
+            override fun onFinish() {
+                Toast.makeText(this@KotlinCatchGame, "Sure doldu", Toast.LENGTH_LONG).show()
+                var warning = AlertDialog.Builder(this@KotlinCatchGame)
+                warning.setTitle("Restart Game")
+                warning.setMessage("Do you want to play one more time ?")
+                warning.setPositiveButton("Yes"){dialog,which->
+
+
+
+                }
+                warning.setNegativeButton("No"){dialog,which->
+                    Toast.makeText(applicationContext,"Game Over",Toast.LENGTH_LONG).show()
+
+
+
+                }
+                warning.show()
+
+
+
+            }
+
+
+        }.start()
+
+
+    }
+    fun increaseScore(view : View){
+        score= score+1
+        scoreText.text ="score : ${score}"
+
+
     }
 }
