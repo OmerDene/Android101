aff3ac118378576b0d64ebc98dde6fca8419ce63
OmerDene <omerdene@gmail.com>
2023-04-04 22:19:42 +0300

    KotlinLandMarkdesign

---
+++ app/src/main/java/com/example/android101/view/KotlinLandMarkApp/KotlinLandMarkApp.kt
@@ -0,0 +1,15 @@
+package com.example.android101.view.KotlinLandMarkApp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.android101.KotlinIntentContext
+import com.example.android101.R
+
+class KotlinLandMarkApp : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_kotlin_land_mark_app)
+
+    }
+}
---
+++ app/src/main/java/com/example/android101/view/KotlinLandMarkApp/KotlinLandMarkDetailActiviy.kt
@@ -0,0 +1,12 @@
+package com.example.android101.view.KotlinLandMarkApp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.android101.R
+
+class KotlinLandMarkDetailActiviy : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_kotlin_land_mark_detail_activiy)
+    }
+}
