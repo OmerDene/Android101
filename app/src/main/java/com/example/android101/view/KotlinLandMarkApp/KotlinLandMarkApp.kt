3a745d1533a9dbe3c115899c4a982e773655e920
OmerDene <omerdene@gmail.com>
2023-04-05 12:32:57 +0300

    landMarkarraylist

--- app/src/main/java/com/example/android101/view/KotlinLandMarkApp/KotlinLandMarkApp.kt
+++ app/src/main/java/com/example/android101/view/KotlinLandMarkApp/KotlinLandMarkApp.kt
@@ -10,11 +10,25 @@ import com.example.android101.databinding.ActivityKotlinLandMarkDetailActiviyBin
 
 class KotlinLandMarkApp : AppCompatActivity() {
     private  lateinit var binding :ActivityKotlinLandMarkAppBinding
+    val arraylist = ArrayList<Landmark>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityKotlinLandMarkAppBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        val pisa = Landmark("Pisa","Italy",R.drawable.pizza)
+        val eifel = Landmark("Eifel","France",R.drawable.eyfel)
+        val collesum = Landmark("Collesum","Italy",R.drawable.collesium)
+        val londonBridge = Landmark("Pisa","Italy",R.drawable.londonbridge)
+
+        arraylist.add(pisa)
+        arraylist.add(eifel)
+        arraylist.add(collesum)
+        arraylist.add(londonBridge)
+
+
+
 
 
 
---
+++ app/src/main/java/com/example/android101/view/KotlinLandMarkApp/Landmark.kt
@@ -0,0 +1,4 @@
+package com.example.android101.view.KotlinLandMarkApp
+
+class Landmark(name : String,country : String,image : Int) {
+}
