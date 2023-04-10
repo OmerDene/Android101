065933c567e32f8a404f3c6d9c8d83c43dfc30b6
OmerDene <omerdene@gmail.com>
2023-04-10 13:30:21 +0300

    KotlinArtBookdesign

---
+++ app/src/main/java/com/example/android101/KotlinArtBook/KotlinArtBookDetail.kt
@@ -0,0 +1,12 @@
+package com.example.android101.KotlinArtBook
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.android101.R
+
+class KotlinArtBookDetail : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_kotlin_art_book_detail)
+    }
+}
