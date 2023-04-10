996f7ed20646ad6434524c1ccc54dd476fcdfdd8
OmerDene <omerdene@gmail.com>
2023-04-10 16:56:51 +0300

    menuInflater

--- app/src/main/java/com/example/android101/KotlinArtBook/KotlinArtBook.kt
+++ app/src/main/java/com/example/android101/KotlinArtBook/KotlinArtBook.kt
@@ -1,12 +1,35 @@
 package com.example.android101.KotlinArtBook
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import com.example.android101.R
+import com.example.android101.databinding.ActivityKotlinArtBookBinding
+import com.example.android101.databinding.ActivityKotlinArtBookDetailBinding
 
 class KotlinArtBook : AppCompatActivity() {
+    private lateinit var binding: ActivityKotlinArtBookBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_kotlin_art_book)
+        binding = ActivityKotlinArtBookBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val menuInflater = menuInflater
+        menuInflater.inflate(R.menu.kotlin_art_menu,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.add_kotlin_art) {
+            val intent = Intent(this,KotlinArtBookDetail::class.java)
+            startActivity(intent)
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
--- app/src/main/java/com/example/android101/KotlinArtBook/KotlinArtBookDetail.kt
+++ app/src/main/java/com/example/android101/KotlinArtBook/KotlinArtBookDetail.kt
@@ -2,11 +2,26 @@ package com.example.android101.KotlinArtBook
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.example.android101.R
+import com.example.android101.databinding.ActivityKotlinArtBookDetailBinding
+import com.example.android101.databinding.ActivityKotlinLandMarkDetailActiviyBinding
 
 class KotlinArtBookDetail : AppCompatActivity() {
+    private  lateinit var binding : ActivityKotlinArtBookDetailBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_kotlin_art_book_detail)
+        binding = ActivityKotlinArtBookDetailBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+    }
+
+
+    fun save(view : View){
+
+
+    }
+    fun fotoSec(view: View){
+
     }
 }
