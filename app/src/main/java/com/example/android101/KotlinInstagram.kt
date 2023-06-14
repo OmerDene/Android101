94a3bdb1ca6c63bb388c613aefed5b0f3b7e3844
OmerDene <omerdene@gmail.com>
2023-06-14 20:04:35 +0300

    clone

---
+++ app/src/main/java/com/example/android101/KotlinInstagram.kt
@@ -0,0 +1,11 @@
+package com.example.android101
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class KotlinInstagram : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_kotlin_instagram)
+    }
+}
