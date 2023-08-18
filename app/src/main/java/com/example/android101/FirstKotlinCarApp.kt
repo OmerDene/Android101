a529075ad95e4622bea4d3cdbaa19cdacbbf3b1a
OmerDene <omerdene@gmail.com>
2023-08-18 15:07:07 +0300

    learningcarAppDesign

---
+++ app/src/main/java/com/example/android101/FirstKotlinCarApp.kt
@@ -0,0 +1,12 @@
+package com.example.android101
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FirstKotlinCarApp : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_first_kotlin_car_app)
+    }
+
+}
