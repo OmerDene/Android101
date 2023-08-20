35b7abd5a9d633219572eba883a26f62ce2777d3
OmerDene <omerdene@gmail.com>
2023-08-20 17:22:36 +0300

    learningViewBinding

--- app/src/main/java/com/example/android101/FirstKotlinCarApp.kt
+++ app/src/main/java/com/example/android101/FirstKotlinCarApp.kt
@@ -6,21 +6,27 @@ import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
-import kotlinx.android.synthetic.main.activity_first_kotlin_car_app.*
+import com.example.android101.databinding.ActivityFirstKotlinCarAppBinding
+
 
 class FirstKotlinCarApp : AppCompatActivity() {
     lateinit var editTextBrand : EditText
     lateinit var editTextColor : EditText
     lateinit var editTextYear : EditText
     lateinit var textShowCarProperties :TextView
+    private lateinit var binding : ActivityFirstKotlinCarAppBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_first_kotlin_car_app)
+        binding = ActivityFirstKotlinCarAppBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
         editTextBrand = findViewById(R.id.editTextCarBrand)
         editTextColor= findViewById(R.id.editTextColor)
         editTextYear = findViewById(R.id.editTextYear)
         textShowCarProperties = findViewById(R.id.textShowCarProperties)
+
 
 
 
