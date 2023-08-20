9f9a189162cf0630bdb5aaf854fca3a8318ed2a3
OmerDene <omerdene@gmail.com>
2023-08-20 17:43:16 +0300

    learningCalculateDesign

---
+++ app/src/main/java/com/example/android101/FirstKotlnCalculateApp.kt
@@ -0,0 +1,25 @@
+package com.example.android101
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class FirstKotlnCalculateApp : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_first_kotln_calculate_app)
+    }
+    fun sum(sum : View) {
+
+    }
+    fun sub(sub : View) {
+
+    }
+    fun multi(multi : View) {
+
+    }
+    fun div(div : View) {
+
+    }
+
+}
