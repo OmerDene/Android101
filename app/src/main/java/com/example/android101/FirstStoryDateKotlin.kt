3a40ee570875cf4148a63bf9ef35aadb4402860e
OmerDene <omerdene@gmail.com>
2023-08-28 19:39:33 +0300

    learningshared

--- app/src/main/java/com/example/android101/FirstStoryDateKotlin.kt
+++ app/src/main/java/com/example/android101/FirstStoryDateKotlin.kt
@@ -1,5 +1,6 @@
 package com.example.android101
 
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -8,13 +9,22 @@ import com.example.android101.databinding.ActivityFirstStoryDateKotlinBinding
 
 class FirstStoryDateKotlin : AppCompatActivity() {
     private lateinit var binding : ActivityFirstStoryDateKotlinBinding
+    private lateinit var sharedPreferences: SharedPreferences
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityFirstStoryDateKotlinBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+        sharedPreferences =this.getSharedPreferences("com.example.android101", MODE_PRIVATE)
     }
     fun save(view : View){
+        val takeAge =binding.editTextFirstKotlinageInput.text.toString().toIntOrNull()
+        if(takeAge != null ) {
+            binding.textViewFirstKotlinageShow.text = "Your age : ${takeAge}"
+        }else {
+            binding.textViewFirstKotlinageShow.text = "please write number"
+        }
+
 
     }
     fun delete(view : View){
