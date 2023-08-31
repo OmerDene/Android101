1da5ff0a2dc7cecd37c3766001061b1aff7589d1
OmerDene <omerdene@gmail.com>
2023-08-31 09:24:27 +0300

    lea

---
+++ app/src/main/java/com/example/android101/FirstAlertDialogKotlin.kt
@@ -0,0 +1,11 @@
+package com.example.android101
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FirstAlertDialogKotlin : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_first_alert_dialog_kotlin)
+    }
+}
