adac92715f17c1a08992cc0a33cbb9fa64469b35
OmerDene <omerdene@gmail.com>
2023-09-07 21:44:04 +0300

    learningintent

--- app/src/main/java/com/example/android101/FirstAlertDialogKotlin.kt
+++ app/src/main/java/com/example/android101/FirstAlertDialogKotlin.kt
@@ -2,10 +2,34 @@ package com.example.android101
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 
 class FirstAlertDialogKotlin : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_first_alert_dialog_kotlin)
+    }
+    fun save(view : View) {
+
+        val alert = AlertDialog.Builder(this)
+        alert.setTitle("Save")
+        alert.setMessage("Are You Sure?")
+
+        alert.setPositiveButton("Yes") {dialog, which ->
+
+            //OnClick Listener
+            Toast.makeText(this,"Saved", Toast.LENGTH_LONG).show()
+        }
+        alert.setNegativeButton("No") {dialog, which ->
+
+            //OnClick Listener
+            Toast.makeText(applicationContext,"Not Saved", Toast.LENGTH_LONG).show()
+
+        }
+
+        alert.show()
+
     }
 }
--- app/src/main/java/com/example/android101/FirstIntentActivityKotlin.kt
+++ app/src/main/java/com/example/android101/FirstIntentActivityKotlin.kt
@@ -2,10 +2,14 @@ package com.example.android101
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class FirstIntentActivityKotlin : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_first_intent_kotlin)
+    }
+    fun next(view : View){
+
     }
 }
