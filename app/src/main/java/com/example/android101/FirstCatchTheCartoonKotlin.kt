24ea419e8f84f7b08e02e56d756aa387e45259ba
OmerDene <omerdene@gmail.com>
2023-09-25 22:42:00 +0300

    increasescore

--- app/src/main/java/com/example/android101/FirstCatchTheCartoonKotlin.kt
+++ app/src/main/java/com/example/android101/FirstCatchTheCartoonKotlin.kt
@@ -2,15 +2,22 @@ package com.example.android101
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.example.android101.databinding.ActivityFirstCatchTheCartoonKotlinBinding
 import com.example.android101.databinding.ActivityKotlinRunnableBinding
 
 class FirstCatchTheCartoonKotlin : AppCompatActivity() {
     private lateinit var binding: ActivityFirstCatchTheCartoonKotlinBinding
+    private var score = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivityFirstCatchTheCartoonKotlinBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         val view = binding.root
         setContentView(view)
+    }
+    fun increaseScore(view : View){
+        score++
+        binding.scoreText.text = "Score : ${score}"
+
     }
 }
