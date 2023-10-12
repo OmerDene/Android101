d2f2a7a5b9414b3927b60fd050dde90460ce51ea
OmerDene <omerdene@gmail.com>
2023-10-12 22:18:49 +0300

    landmarkdesign

---
+++ app/src/main/java/com/example/android101/LandMarkBookFirstKotlin.kt
@@ -0,0 +1,11 @@
+package com.example.android101
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LandMarkBookFirstKotlin : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_land_mark_book_first_kotlin)
+    }
+}
---
+++ app/src/main/java/com/example/android101/LandMarkDetailsActivityFirstKotlin.kt
@@ -0,0 +1,14 @@
+package com.example.android101
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LandMarkDetailsActivityFirstKotlin : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_land_mark_details_first_kotlin)
+        var omer = LandMarkDetailsActivityFirstKotlin()
+
+
+    }
+}
