707be34ebb282b52daae06a262946a4e00338671
OmerDene <omerdene@gmail.com>
2023-10-18 22:41:19 +0300

    recyclerView

---
+++ app/src/main/java/com/example/android101/LandmarkAdapterKotlin.kt
@@ -0,0 +1,23 @@
+package com.example.android101
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class LandmarkAdapterKotlin : RecyclerView.Adapter<LandmarkAdapterKotlin.LandmarkHolder>() {
+    class LandmarkHolder(itemView : View) : RecyclerView.ViewHolder(itemView) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LandmarkHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: LandmarkHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
