f36f7aeca9e26846c280c5f4c7ae2b832876e719
OmerDene <omerdene@gmail.com>
2023-10-26 16:52:29 +0300

    artKotlinDesign

---
+++ app/src/main/java/com/example/android101/ArtBookFirstKotlin.kt
@@ -0,0 +1,11 @@
+package com.example.android101
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ArtBookFirstKotlin : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_art_book_first_kotlin)
+    }
+}
---
+++ app/src/main/java/com/example/android101/ArtBookFirstKotlinDetails.kt
@@ -0,0 +1,18 @@
+package com.example.android101
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class ArtBookFirstKotlinDetails : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_art_book_first_kotlin_details)
+    }
+    fun save (view : View){
+
+    }
+    fun selectedPicture(view : View){
+
+    }
+}
