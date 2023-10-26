83c3738da7f898a8c56f3054c3b53f9a1f8bb7bf
OmerDene <omerdene@gmail.com>
2023-10-26 19:40:55 +0300

    recyclerviewrow

--- app/src/main/java/com/example/android101/ArtBookFirstKotlin.kt
+++ app/src/main/java/com/example/android101/ArtBookFirstKotlin.kt
@@ -2,10 +2,16 @@ package com.example.android101
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.android101.databinding.ActivityArtBookFirstKotlinBinding
+import com.example.android101.databinding.ActivityArtBookFirstKotlinDetailsBinding
 
 class ArtBookFirstKotlin : AppCompatActivity() {
+    private lateinit var binding : ActivityArtBookFirstKotlinBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_art_book_first_kotlin)
+        binding = ActivityArtBookFirstKotlinBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
     }
 }
--- app/src/main/java/com/example/android101/ArtBookFirstKotlinDetails.kt
+++ app/src/main/java/com/example/android101/ArtBookFirstKotlinDetails.kt
@@ -3,13 +3,18 @@ package com.example.android101
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import com.example.android101.databinding.ActivityArtBookFirstKotlinDetailsBinding
 
 class ArtBookFirstKotlinDetails : AppCompatActivity() {
+    private lateinit var binding : ActivityArtBookFirstKotlinDetailsBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_art_book_first_kotlin_details)
+        binding = ActivityArtBookFirstKotlinDetailsBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
     }
     fun save (view : View){
+
 
     }
     fun selectedPicture(view : View){
