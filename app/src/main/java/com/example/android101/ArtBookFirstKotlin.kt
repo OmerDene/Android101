3f9967967ddd6dd56013d2c571dee5850495bb3e
OmerDene <omerdene@gmail.com>
2023-10-28 23:53:50 +0300

    menuinflate

--- app/src/main/java/com/example/android101/ArtBookFirstKotlin.kt
+++ app/src/main/java/com/example/android101/ArtBookFirstKotlin.kt
@@ -1,7 +1,10 @@
 package com.example.android101
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import com.example.android101.databinding.ActivityArtBookFirstKotlinBinding
 import com.example.android101.databinding.ActivityArtBookFirstKotlinDetailsBinding
 
@@ -13,5 +16,19 @@ class ArtBookFirstKotlin : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val menuInflater = menuInflater
+        menuInflater.inflate(R.menu.art_kotlin_first_menu,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(item.itemId == R.id.add_art_item_kotlin){
+            val intent = Intent(this@ArtBookFirstKotlin,ActivityArtBookFirstKotlinDetailsBinding::class.java)
+            startActivity(intent)
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
